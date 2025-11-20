71f97d8764d604fd18f873d65b4c3f541b0137ca
Billy Gilchrist <billygilchrist@MacBookPro.lan>
2025-11-20 12:50:52 +0000

    Use escaped private key format

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ import admin from "firebase-admin";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
-}),
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      // Convert \n sequences in the env var into real newlines
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
